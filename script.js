52eddc14cd50e6e60a7239c3d4e9d856513d046e
Mizael Alves <mizaelbna@hotmail.com>
2021-07-16 20:31:44 -0300

    e-commerce tamplate

---
+++ script.js
@@ -0,0 +1,33 @@
+function cadastraUsuario() {
+  var usuarioNome = document.getElementById("fname");
+  var usuarioSobrenome = document.getElementById("lname");
+  var usuarioEmail = document.getElementById("email");
+
+  var dados = JSON.parse(localStorage.getItem("dadosUsuario"));
+  if (dados == null) {
+    localStorage.setItem("dadosUsuario", "[]");
+    dados = [];
+  }
+
+  if (usuarioNome.value == "" || usuarioSobrenome.value == ""  || usuarioEmail.value == "") {
+    alert("Preencha o campo");
+    return false;
+  } else {
+    var auxRegistro = {
+      nome: usuarioNome.value,
+      sobrenome: usuarioSobrenome.value,
+      email: usuarioEmail.value,
+    };
+
+    dados.push(auxRegistro);
+
+    localStorage.setItem("dadosUsuario", JSON.stringify(dados));
+    console.log("Usuario " + auxRegistro.nome + " cadastrado");
+
+    alert("Usuario "+ auxRegistro.nome +" cadastrado");
+
+    usuarioNome.value = "";
+    usuarioSobrenome.value = "";
+    usuarioEmail.value = "";
+  }
+}
